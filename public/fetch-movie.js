32e102f6bb08fa48a96f479e45adcf1663a67d6c
Ekiram <ekiram.salih@edu.newton.se>
2022-09-22 20:19:24 +0200

    Update

---
+++ public/fetch-movie.js
@@ -0,0 +1,27 @@
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+    return parent.appendChild(el);
+}
+
+const ul = document.getElementById('movie');
+
+
+const url = 'http://localhost:3000/api/movies';
+
+//const url = 'data/data.json';
+fetch(url)
+    .then((resp) => resp.json())
+    .then(function(data) {
+        console.log(data.movie);
+        let movie = data.movie;
+        return movie.map(function(movie)
+        {
+            let li = createNode('li');
+            li.innerHTML = "<img src ='img/" + movie.movieImg + "' class='img'>" + '<p></p>' + movie.movieDescription;
+            append(ul, li);
+
+        })
+    })
